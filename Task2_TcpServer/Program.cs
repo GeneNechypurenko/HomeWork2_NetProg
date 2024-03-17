e0c9539070b90ba5f39f64f6a76ba15f52c7d570
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-03-17 17:40:36 +0200

    complete

---
+++ Task2_TcpServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Diagnostics;
+
+namespace Task2_TaskManagerTcpServer
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 12345);
+            server.Start();
+            Console.WriteLine("Server started. Waiting for clients...");
+
+            while (true)
+            {
+                using (TcpClient client = server.AcceptTcpClient())
+                using (NetworkStream stream = client.GetStream())
+                {
+                    string processList = GetRunningProcesses();
+                    byte[] data = Encoding.UTF8.GetBytes(processList);
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+        }
+        private static string GetRunningProcesses()
+        {
+            Process[] processes = Process.GetProcesses();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Process process in processes)
+            {
+                sb.AppendLine(process.ProcessName);
+            }
+            return sb.ToString();
+        }
+    }
+}
